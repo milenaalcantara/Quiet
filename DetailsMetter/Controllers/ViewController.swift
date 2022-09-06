b39e187a22453f00618b7a58e9b1c58b417c2a6d
Milena Alcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2022-09-06 16:01:58 -0300

    configure initial project

---
+++ DetailsMetter/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DetailsMetter
+//
+//  Created by Milena Lima de Alcântara on 06/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
