e8764c275cd64fbc1ec796e58e9660fa40318ee7
Milena Alcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2022-09-29 09:07:56 -0300

    add sound background button

---
+++ DetailsMetter/View/InfoView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoView.swift
+//  DetailsMetter
+//
+//  Created by Milena Lima de Alcântara on 26/09/22.
+//
+
+import UIKit
+
+class InfoView: UIView {
+    
+    var titleView: UILabel = {
+        let label = UILabel()
+        label.text = ""
+        return label
+    }()
+
+}
