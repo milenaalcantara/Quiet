1e147aa5ace942f16ae2c345aea027c162639595
Moyses Azevedo <m.mirazev@gmail.com>
2023-02-13 11:10:32 -0300

    :sparkles: Add OnboardView

--- Quiet/View/OnboardView.swift
+++ Quiet/View/OnboardView.swift
@@ -8,6 +8,18 @@
 import UIKit
 
 class OnboardView: UIView {
+    var skipAction: (() -> Void)?
+
+    lazy var skipButton: UIButton = {
+        let skipButton = UIButton()
+        skipButton.setTitle("Ir para a próxima etapa", for: .normal)
+        skipButton.backgroundColor = .tintColor
+        skipButton.layer.cornerRadius = 10
+        skipButton.translatesAutoresizingMaskIntoConstraints = false
+        skipButton.addTarget(self, action: #selector(skipButtonAction), for: .touchUpInside)
+        return skipButton
+    }()
+
 
     /*
     // Only override draw() if you perform custom drawing.
@@ -16,5 +28,40 @@ class OnboardView: UIView {
         // Drawing code
     }
     */
+    override func willMove(toWindow newWindow: UIWindow?) {
+        super.willMove(toWindow: newWindow)
+        setupView()
+        setConstraints()
+    }
+}
 
+
+extension OnboardView {
+    // ℹ️ Button action func:
+    @objc func skipButtonAction() {
+        skipAction?()
+    }
+}
+
+
+extension OnboardView {
+    func setupView() {
+        backgroundColor = .systemBackground
+        self.addSubview(skipButton)
+    }
+}
+
+extension OnboardView {
+    private func setConstraints() {
+        skipButtonContraints()
+    }
+
+    private func skipButtonContraints() {
+        NSLayoutConstraint.activate([
+            skipButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -44),
+            skipButton.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.05),
+            skipButton.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.8),
+            skipButton.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+    }
 }
