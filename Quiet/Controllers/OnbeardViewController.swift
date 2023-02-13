3f95f773d4864716d47ce846461bcb791d616667
Moyses Azevedo <m.mirazev@gmail.com>
2023-02-13 11:11:55 -0300

    :sparkles: Add OnboardViewController

--- Quiet/Controllers/OnbeardViewController.swift
+++ Quiet/Controllers/OnbeardViewController.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import UIKit
+
+
+class OnboardViewController: UIViewController {
+    private var contentView = OnboardView()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func loadView() {
+        view = contentView
+        contentView.skipAction = nextView
+    }
+}
+
+
+extension OnboardViewController {
+    func nextView() {
+        self.navigationController?.pushViewController(BeachViewController(), animated: false)
+    }
+}
