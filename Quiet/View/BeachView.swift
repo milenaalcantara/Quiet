ea7789a0c45f9d1b59bf2adc898bc819ac479c74
Moyses Azevedo <m.mirazev@gmail.com>
2023-02-24 11:49:28 -0300

    :recycle: refactory constraints

--- Quiet/View/BeachView.swift
+++ Quiet/View/BeachView.swift
@@ -81,31 +81,7 @@ class BeachView: UIView {
         
         return sunAsset
     }()
-    
-    lazy var starAsset: UIImageView = {
-        let starAsset = UIImageView(image: UIImage (named: "star"))
-        starAsset.translatesAutoresizingMaskIntoConstraints = false
-        starAsset.isAccessibilityElement = true
-        starAsset.accessibilityTraits = .button
-        starAsset.accessibilityLabel = "Sem som"
-        starAsset.accessibilityValue = isPlaying ? "Ligado" : "Desligado"
-        starAsset.accessibilityHint = "sem som"
-        
-        return starAsset
-    }()
-    
-    lazy var shellAsset: UIImageView = {
-        let shellAsset = UIImageView(image: UIImage(named: "shell"))
-        shellAsset.translatesAutoresizingMaskIntoConstraints = false
-        shellAsset.isAccessibilityElement = true
-        shellAsset.accessibilityTraits = .button
-        shellAsset.accessibilityLabel = "Sem som"
-        shellAsset.accessibilityValue = isPlaying ? "Ligado" : "Desligado"
-        shellAsset.accessibilityHint = "sem som"
 
-        return shellAsset
-    }()
-    
     lazy var oceanAsset: LottieAnimationView = {
         let oceanView: LottieAnimationView
         oceanView = .init(name: "ocean-gradient")
@@ -120,7 +96,7 @@ class BeachView: UIView {
 
         oceanView.addGestureRecognizer(tapPlayGesture)
         oceanView.isUserInteractionEnabled = true
-        
+
         boatAsset.accessibilityTraits = .button
         boatAsset.accessibilityLabel = "Oceano"
         boatAsset.accessibilityValue = isPlaying ? "Ligado" : "Desligado"
@@ -128,7 +104,7 @@ class BeachView: UIView {
 
         return oceanView
     }()
-    
+
     lazy var boatAsset: UIImageView = {
         let boatAsset = UIImageView(image: UIImage(named: "boat"))
         boatAsset.translatesAutoresizingMaskIntoConstraints = false
@@ -139,8 +115,6 @@ class BeachView: UIView {
         return boatAsset
     }()
 
-   
-
     lazy var coconutTreeAsset: LottieAnimationView = {
         let coconutTreeAsset: LottieAnimationView
         coconutTreeAsset = .init(name: "coqueiro")
@@ -148,24 +122,48 @@ class BeachView: UIView {
         coconutTreeAsset.play()
         coconutTreeAsset.contentMode = .scaleToFill
         coconutTreeAsset.translatesAutoresizingMaskIntoConstraints = false
-        
+
         let tapPlayGesture = UITapGestureRecognizer(target: self, action: #selector(didTapCoconutTreeAsset(_:)))
         tapPlayGesture.numberOfTapsRequired = 1
         tapPlayGesture.numberOfTouchesRequired = 1
-        
         coconutTreeAsset.addGestureRecognizer(tapPlayGesture)
         coconutTreeAsset.isUserInteractionEnabled = true
-        
+
         coconutTreeAsset.accessibilityTraits = .button
         coconutTreeAsset.accessibilityLabel = "Coqueiro"
         coconutTreeAsset.accessibilityValue = isPlaying ? "Ligado" : "Desligado"
         coconutTreeAsset.accessibilityHint = "clique duas vezes para som de vento"
-        
+
         // coconut voice
-        
-        
+
         return coconutTreeAsset
     }()
+
+    lazy var shellAsset: UIImageView = {
+        let shellAsset = UIImageView(image: UIImage(named: "shell"))
+        shellAsset.translatesAutoresizingMaskIntoConstraints = false
+        shellAsset.isAccessibilityElement = true
+        shellAsset.accessibilityTraits = .button
+        shellAsset.accessibilityLabel = "Sem som"
+        shellAsset.accessibilityValue = isPlaying ? "Ligado" : "Desligado"
+        shellAsset.accessibilityHint = "sem som"
+
+        return shellAsset
+    }()
+    
+    lazy var starAsset: UIImageView = {
+        let starAsset = UIImageView(image: UIImage (named: "star"))
+        starAsset.translatesAutoresizingMaskIntoConstraints = false
+        starAsset.isAccessibilityElement = true
+        starAsset.accessibilityTraits = .button
+        starAsset.accessibilityLabel = "Sem som"
+        starAsset.accessibilityValue = isPlaying ? "Ligado" : "Desligado"
+        starAsset.accessibilityHint = "sem som"
+        
+        return starAsset
+    }()
+
+
 
     override func willMove(toWindow newWindow: UIWindow?) {
         super.willMove(toWindow: newWindow)
@@ -210,14 +208,15 @@ extension BeachView {
 extension BeachView: ViewCodingProtocol {
     func setupHierarchy() {
         addSubview(backgroundImageView)
-        addSubview(oceanAsset)
-        addSubview(sunAsset)
-        addSubview(shellAsset)
-        addSubview(boatAsset)
-        addSubview(coconutTreeAsset)
-        addSubview(starAsset)
         addSubview(infoButton)
         addSubview(soundButton)
+        addSubview(sunAsset)
+        addSubview(oceanAsset)
+        addSubview(boatAsset)
+        addSubview(coconutTreeAsset)
+        addSubview(shellAsset)
+        addSubview(starAsset)
+
     }
 
     func setupConstraints() {
@@ -236,85 +235,83 @@ extension BeachView: ViewCodingProtocol {
 private extension BeachView {
     func backgroundImageViewConstraints() {
         NSLayoutConstraint.activate([
-            backgroundImageView.topAnchor.constraint(equalTo: topAnchor),
-            backgroundImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            backgroundImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            backgroundImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            ])
+            backgroundImageView.heightAnchor.constraint(equalTo: heightAnchor),
+            backgroundImageView.widthAnchor.constraint(equalTo: widthAnchor, constant: 4),
+            backgroundImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            backgroundImageView.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
     }
 
     func infoButtonConstraints() {
         NSLayoutConstraint.activate([
-        infoButton.topAnchor.constraint(equalTo: topAnchor, constant: 76),
-        infoButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
-        infoButton.widthAnchor.constraint(equalToConstant: 40),
-        infoButton.heightAnchor.constraint(equalToConstant: 40)
+            infoButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            infoButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: frame.width * 0.01),
+            infoButton.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.20),
+            infoButton.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 0.20)
         ])
     }
 
     func soundButtonConstraints() {
         NSLayoutConstraint.activate([
-        soundButton.topAnchor.constraint(equalTo: topAnchor, constant: 76),
-        soundButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
-        soundButton.widthAnchor.constraint(equalToConstant: 40),
-        soundButton.heightAnchor.constraint(equalToConstant: 40)
+            soundButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            soundButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: frame.width * -0.01),
+            soundButton.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.20),
+            soundButton.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 0.20)
         ])
     }
 
     func oceanAssetConstraints() {
         NSLayoutConstraint.activate([
             oceanAsset.centerXAnchor.constraint(equalTo: centerXAnchor),
-            oceanAsset.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 305),
-            oceanAsset.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.width/1.4),
-            oceanAsset.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width+10),
-            oceanAsset.bottomAnchor.constraint(equalTo: shellAsset.topAnchor, constant: -70),
-            oceanAsset.bottomAnchor.constraint(equalTo: coconutTreeAsset.topAnchor, constant: 130),
-            oceanAsset.bottomAnchor.constraint(equalTo: coconutTreeAsset.topAnchor, constant: 130),
-            oceanAsset.bottomAnchor.constraint(equalTo: starAsset.topAnchor, constant: -40),
+            oceanAsset.centerYAnchor.constraint(equalTo: centerYAnchor, constant: frame.height * 0.10),
+            oceanAsset.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.30),
+            oceanAsset.widthAnchor.constraint(equalTo: widthAnchor, constant: 2)
             ])
     }
 
     func sunAssetConstraints() {
         NSLayoutConstraint.activate([
-            sunAsset.bottomAnchor.constraint(equalTo: oceanAsset.topAnchor, constant: -80 ),
-            sunAsset.heightAnchor.constraint(equalToConstant: 140),
-            sunAsset.widthAnchor.constraint(equalToConstant: 150),
-            sunAsset.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            sunAsset.topAnchor.constraint(equalTo: topAnchor, constant: frame.height * 0.15),
+            sunAsset.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.15),
+            sunAsset.widthAnchor.constraint(equalTo: heightAnchor, multiplier: 0.15),
+            sunAsset.leadingAnchor.constraint(equalTo: leadingAnchor, constant: frame.height * 0.05),
+        ])
+    }
+
+    func boatAssetConstraints() {
+        NSLayoutConstraint.activate([
+            boatAsset.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 0.25),
+            boatAsset.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.25),
+            boatAsset.trailingAnchor.constraint(equalTo: starAsset.trailingAnchor),
+            boatAsset.topAnchor.constraint(equalTo: centerYAnchor, constant: frame.height * -0.10)
+        ])
+    }
+
+    func coconutTreeAssetConstraints() {
+        NSLayoutConstraint.activate([
+            coconutTreeAsset.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.30),
+            coconutTreeAsset.widthAnchor.constraint(equalTo: heightAnchor, multiplier: 0.20),
+            coconutTreeAsset.centerXAnchor.constraint(equalTo: sunAsset.centerXAnchor),
+            coconutTreeAsset.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: frame.height * -0.06)
         ])
     }
 
     func shellAssetConstrainst() {
         NSLayoutConstraint.activate([
-            shellAsset.heightAnchor.constraint(equalToConstant: 30),
-            shellAsset.widthAnchor.constraint(equalToConstant: 30),
-            shellAsset.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 60),
+            shellAsset.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 0.10),
+            shellAsset.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.10),
+            shellAsset.trailingAnchor.constraint(equalTo: boatAsset.leadingAnchor),
+            shellAsset.bottomAnchor.constraint(equalTo: bottomAnchor, constant: frame.height * -0.15)
             ])
-    }
-
-    func boatAssetConstraints() {
-        NSLayoutConstraint.activate([
-            boatAsset.bottomAnchor.constraint(equalTo: oceanAsset.topAnchor, constant: 50 ),
-            boatAsset.heightAnchor.constraint(equalToConstant: 80),
-            boatAsset.widthAnchor.constraint(equalToConstant: 80),
-            boatAsset.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 35),
-        ])
-    }
-
-
-    func coconutTreeAssetConstraints() {
-        NSLayoutConstraint.activate([
-        coconutTreeAsset.heightAnchor.constraint(equalToConstant: 250),
-        coconutTreeAsset.widthAnchor.constraint(equalToConstant: 178),
-        coconutTreeAsset.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 40),
-        ])
     }
 
     func starAssetConstraints() {
         NSLayoutConstraint.activate([
-        starAsset.heightAnchor.constraint(equalToConstant: 30),
-        starAsset.widthAnchor.constraint(equalToConstant: 30),
-        starAsset.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 160)
+            starAsset.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 0.08),
+            starAsset.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.08),
+            starAsset.trailingAnchor.constraint(equalTo: trailingAnchor, constant: frame.width * -0.10),
+            starAsset.bottomAnchor.constraint(equalTo: coconutTreeAsset.bottomAnchor, constant: -10)
         ])
-
     }
+
 }
