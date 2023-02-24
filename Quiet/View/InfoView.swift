7c4038d413df5594cfc3beaa366bce1dafea4f9b
milenaalcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2023-02-24 08:38:29 -0300

    :sparkles: add text

--- Quiet/View/InfoView.swift
+++ Quiet/View/InfoView.swift
@@ -16,19 +16,25 @@ class InfoView: UIView {
         return imageView
     }()
     
-    lazy var titleLabel: UILabel = {
+    lazy var aboutAppText: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Sobre o Quiet"
+        label.text = """
+            O Quiet propõe uma experiência sensorial por meio de uma interface simples e imersiva. Nele você pode explorar os diferentes elementos da tela para interagir com os sons naturais cuidadosamente selecionados para oferecer a experiência relaxante de uma praia.
+
+            Esperamos que você aproveite ao máximo a sua jornada conosco.
+        """
         label.textColor = .label
-        label.font = .systemFont(ofSize: 16)
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 17)
         
         return label
     }()
     
     override func layoutSubviews() {
         self.addSubview(backgroundImage)
-        self.addSubview(titleLabel)
+        self.addSubview(aboutAppText)
         configureConstraints()
     }
     
@@ -38,8 +44,19 @@ class InfoView: UIView {
             backgroundImage.bottomAnchor.constraint(equalTo: self.bottomAnchor),
             backgroundImage.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             backgroundImage.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+            aboutAppText.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+//            aboutAppText.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            aboutAppText.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.9),
+            aboutAppText.topAnchor.constraint(equalTo: self.topAnchor, constant: 70)
         ])
     }
 }
+ /*
+  Seja bem-vindo ao Quiet - o seu refúgio tranquilo para relaxar e desestressar. Nós acreditamos que todos merecem um momento de paz e serenidade em meio ao caos do dia a dia. É por isso que criamos este aplicativo sensorial com sons naturais cuidadosamente selecionados para proporcionar uma experiência imersiva e revigorante.
+
+  Ao abrir o Quiet, você será levado diretamente para a praia, com o som calmante das ondas do mar e a brisa suave do vento acariciando seu rosto. Você pode explorar os diferentes elementos da tela, cada um com um som distinto, incluindo o som das gaivotas, o barulho do mar agitado, o ruído suave de um barco a vela navegando e o sussurro tranquilo do vento. Todos esses sons trabalham juntos para criar uma sinfonia natural que acalma a mente e relaxa o corpo.
+
+  Nós projetamos o Quiet para ser uma experiência inclusiva para todos, independentemente de suas preferências ou necessidades sensoriais. Com uma interface simples e intuitiva, o aplicativo pode ser facilmente personalizado para atender às suas necessidades únicas. Se você está procurando um ambiente tranquilo para meditar, dormir ou simplesmente relaxar, o Quiet é o lugar perfeito para você.
+
+  Obrigado por escolher o Quiet como seu companheiro para o relaxamento e a serenidade. Esperamos que você aproveite ao máximo a sua jornada sensorial conosco. Relaxe, respire profundamente e deixe os sons naturais do Quiet levá-lo a um lugar de paz interior.
+  */
