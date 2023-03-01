391cd6cdc990f594798e4bed91f934557ffe5514
milenaalcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2023-03-01 08:33:33 -0300

    :sparkles: feat: add content in infoView

--- Quiet/View/InfoView.swift
+++ Quiet/View/InfoView.swift
@@ -16,47 +16,58 @@ class InfoView: UIView {
         return imageView
     }()
     
-    lazy var aboutAppText: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = """
-            O Quiet propõe uma experiência sensorial por meio de uma interface simples e imersiva. Nele você pode explorar os diferentes elementos da tela para interagir com os sons naturais cuidadosamente selecionados para oferecer a experiência relaxante de uma praia.
-
+    lazy var aboutSection: SectionView = {
+        let section = SectionView(
+            title: "Sobre o app",
+            bodyText: """
+              O Quiet propõe uma experiência sensorial por meio de uma interface simples e imersiva. Nele você pode explorar os diferentes elementos da tela para interagir com os sons naturais cuidadosamente selecionados para oferecer a experiência relaxante de uma praia.
+            
             Esperamos que você aproveite ao máximo a sua jornada conosco.
-        """
-        label.textColor = .label
-        label.numberOfLines = 0
-        label.textAlignment = .left
-        label.font = .systemFont(ofSize: 17)
+            """)
+        section.translatesAutoresizingMaskIntoConstraints = false
         
-        return label
+        return section
     }()
     
+    lazy var creditSection: SectionView = {
+        let section = SectionView(
+            title: "Sons",
+            bodyText: """
+            Efeitos sonoros obtidos de:
+            
+            • https://www.zapsplat.com/
+            • https://pixabay.com/
+            """
+//            isNeedTable: true
+        )
+        section.translatesAutoresizingMaskIntoConstraints = false
+
+        return section
+    }()
+
     override func layoutSubviews() {
-        self.addSubview(backgroundImage)
-        self.addSubview(aboutAppText)
+        addSubview(backgroundImage)
+        addSubview(aboutSection)
+        addSubview(creditSection)
         configureConstraints()
     }
     
     func configureConstraints() {
         NSLayoutConstraint.activate([
-            backgroundImage.topAnchor.constraint(equalTo: self.topAnchor),
-            backgroundImage.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            backgroundImage.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            backgroundImage.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            aboutAppText.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-//            aboutAppText.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            aboutAppText.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.9),
-            aboutAppText.topAnchor.constraint(equalTo: self.topAnchor, constant: 70)
+            backgroundImage.topAnchor.constraint(equalTo: topAnchor),
+            backgroundImage.bottomAnchor.constraint(equalTo: bottomAnchor),
+            backgroundImage.leadingAnchor.constraint(equalTo: leadingAnchor),
+            backgroundImage.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            aboutSection.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10),
+            aboutSection.centerXAnchor.constraint(equalTo: centerXAnchor),
+            aboutSection.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.9),
+            aboutSection.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 0.78),
+            
+            creditSection.topAnchor.constraint(equalTo: aboutSection.bottomAnchor, constant: 20),
+            creditSection.centerXAnchor.constraint(equalTo: centerXAnchor),
+            creditSection.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.9),
+            creditSection.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 0.4),
         ])
     }
 }
- /*
-  Seja bem-vindo ao Quiet - o seu refúgio tranquilo para relaxar e desestressar. Nós acreditamos que todos merecem um momento de paz e serenidade em meio ao caos do dia a dia. É por isso que criamos este aplicativo sensorial com sons naturais cuidadosamente selecionados para proporcionar uma experiência imersiva e revigorante.
-
-  Ao abrir o Quiet, você será levado diretamente para a praia, com o som calmante das ondas do mar e a brisa suave do vento acariciando seu rosto. Você pode explorar os diferentes elementos da tela, cada um com um som distinto, incluindo o som das gaivotas, o barulho do mar agitado, o ruído suave de um barco a vela navegando e o sussurro tranquilo do vento. Todos esses sons trabalham juntos para criar uma sinfonia natural que acalma a mente e relaxa o corpo.
-
-  Nós projetamos o Quiet para ser uma experiência inclusiva para todos, independentemente de suas preferências ou necessidades sensoriais. Com uma interface simples e intuitiva, o aplicativo pode ser facilmente personalizado para atender às suas necessidades únicas. Se você está procurando um ambiente tranquilo para meditar, dormir ou simplesmente relaxar, o Quiet é o lugar perfeito para você.
-
-  Obrigado por escolher o Quiet como seu companheiro para o relaxamento e a serenidade. Esperamos que você aproveite ao máximo a sua jornada sensorial conosco. Relaxe, respire profundamente e deixe os sons naturais do Quiet levá-lo a um lugar de paz interior.
-  */
